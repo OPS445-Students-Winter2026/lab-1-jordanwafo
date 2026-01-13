40453d70cd145ee0e40c5b1389c7bd203bbc0aaf
Mark Jordan Ndolla Wafo <mjndolla-wafo@myseneca.ca>
2026-01-13 15:33:17 -0500

    update

---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: This program performs math operations using integer objects'''
+
+x = 10
+y = 2
+z = 5
+
+# Perform the math and print
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(x + y * z))
